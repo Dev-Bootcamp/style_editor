9b5ebbc3e27c160349bcdaf51fb1cb4a5c2f36cd
dbc-apprentice <apprentice@devbootcamp.com>
2013-09-30 12:02:38 -0500

    working

--- style.js
+++ style.js
@@ -1,4 +1,14 @@
-// Shorthand for document ready
+
 $(function(){
-  // your code goes here.
+  $('#style_editor input:nth-child(4)').click(function(e) {
+    e.preventDefault();
+
+    var selector = $('#style_editor input:nth-child(1)').val();
+    var property = $('#style_editor input:nth-child(2)').val();
+    var value = $('#style_editor input:nth-child(3)').val();
+    console.log(selector);
+    console.log(property);
+    console.log(value);
+    $(selector).css(property, value);
+  });
 });
