ecafce33edd69cbe15b316435de125c9ac57c42e
GeoffCrittenden <geoffcritt@hotmail.com>
2013-09-30 17:44:55 -0500

    Update style.js
    cleanup of some test code

--- style.js
+++ style.js
@@ -1,4 +1,3 @@
-
 $(function(){
   $('#style_editor input:nth-child(4)').click(function(e) {
     e.preventDefault();
@@ -6,9 +5,7 @@ $(function(){
     var selector = $('#style_editor input:nth-child(1)').val();
     var property = $('#style_editor input:nth-child(2)').val();
     var value = $('#style_editor input:nth-child(3)').val();
-    console.log(selector);
-    console.log(property);
-    console.log(value);
+    
     $(selector).css(property, value);
   });
 });
